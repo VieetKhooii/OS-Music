acccf59c1b5a5a642c739015028db3dd66756f77
VieetKhooii <nguyenkhooii03@gmail.com>
2024-03-30 11:48:08 +0700

    gitignore

--- Server/data_config.py
+++ Server/data_config.py
@@ -6,7 +6,7 @@ def connect_mysql():
         connection = mysql.connector.connect(host='localhost',
                                             database='music',
                                             user='root',
-                                            password='')
+                                            password=' ')
         if connection.is_connected():
             db_Info = connection.get_server_info()
             cursor = connection.cursor()
