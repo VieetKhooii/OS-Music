3a1af4835b787e28422cd5890e3d476c6a806edd
dophuochung2428 <dophuochung2428@gmail.com>
2024-03-30 11:02:22 +0700

    new version

--- Server/dto/Music.py
+++ Server/dto/Music.py
@@ -1,6 +1,7 @@
 class Music:
-    def __init__(self, id:str, name:str, artist:str, link:str):
+    def __init__(self, id:str, name:str, artist:str, link:str, img:str):
         self.id = id
         self.name = name
         self.artist = artist
         self.link = link
+        self.img = img
