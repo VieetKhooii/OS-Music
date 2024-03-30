2fe4a579b42c54626d36f15cecb85302edd23a53
dophuochung2428 <dophuochung2428@gmail.com>
2024-03-30 10:53:46 +0700

    upload dto

---
+++ Server/dto/Music.py
@@ -0,0 +1,6 @@
+class Music:
+    def __init__(self, id:str, name:str, artist:str, link:str):
+        self.id = id
+        self.name = name
+        self.artist = artist
+        self.link = link
---
+++ Server/dto/Playlist.py
@@ -0,0 +1,5 @@
+class Music:
+    def __init__(self, id:str, name:str, create_date:str):
+        self.id = id
+        self.name = name
+        self.create_date = create_date
